c8344ca4e035282cd15d4af97ee533a9f078dc8e
Jitulteron7 <jitulteron9@gmail.com>
2024-02-18 19:37:24 +0530

    chore: redux for editor state added

---
+++ src/redux/feature/editor.tsx
@@ -0,0 +1,85 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+export interface EditorState {
+  value: {
+    innerElement: EditorInnerElement;
+    editorWrapper: EditorWrapper;
+  };
+}
+
+export interface EditorInnerElement {
+  origin: {
+    x: number;
+    y: number;
+  };
+  styles: {
+    height: number;
+    width: number;
+  };
+}
+
+export interface EditorWrapper {
+  isSelected: boolean;
+}
+
+const initialState: EditorState = {
+  value: {
+    innerElement: {
+      origin: {
+        x: 0,
+        y: 0,
+      },
+      styles: {
+        height: 100,
+        width: 100,
+      },
+    },
+    editorWrapper: {
+      isSelected: false,
+    },
+  },
+};
+
+export const editorSlice = createSlice({
+  name: 'editor',
+  initialState,
+  reducers: {
+    innerElementStyle: (
+      state,
+      action: PayloadAction<Partial<EditorInnerElement>>
+    ) => {
+      console.log(action.payload.styles, 'stylse');
+
+      state.value.innerElement = {
+        ...state.value.innerElement,
+        styles: {
+          height: action.payload.styles?.height as number,
+          width: action.payload.styles?.height as number,
+        },
+      };
+    },
+
+    innerElementOrigin: (
+      state,
+      action: PayloadAction<Partial<EditorInnerElement>>
+    ) => {
+      state.value.innerElement = {
+        ...state.value.innerElement,
+        origin: {
+          x: action.payload.origin?.x as number,
+          y: action.payload.origin?.y as number,
+        },
+      };
+    },
+    isSelectedWrapper: (state) => {
+      const orgState = state.value.editorWrapper.isSelected;
+      state.value.editorWrapper.isSelected = !orgState;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { isSelectedWrapper, innerElementStyle } = editorSlice.actions;
+
+export default editorSlice.reducer;
